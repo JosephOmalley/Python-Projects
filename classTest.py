36ac1b4e977966cf83084e12e25d23bc16ca2311
Joe <josephomalley135@gmail.com>
2021-03-11 20:19:40 -0600

    Update classTest.py

--- classTest.py
+++ classTest.py
@@ -1,13 +1,13 @@
-class User:
+class User: # create parent class 
     name = "Blank"
     email = " "
     password = "Blank"
     account_number = 0
-
+    #create child class
 class Teacher(User):
     subject = "History"
     gradelevel = 11
-    
+    #create child class
 class Student(User):
     GPA = 4.0
     GradYear = 2025
