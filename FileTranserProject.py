8501f8cc39a74bdf574e55ff3f8a630b5038df52
Joe <josephomalley135@gmail.com>
2021-03-27 19:43:21 -0500

    Update

--- FileTranserProject.py
+++ FileTranserProject.py
@@ -1,41 +1,29 @@
+# Upon execution of this program all files that were modified in the past 24 hours will be transfered to recieving folder
+# 3.9.0 version of python
+# author Joseph Omalley
+
+
 import shutil
 import os.path as path
 import datetime
-import timedelta
+from datetime import timedelta
 import time
 import os
 #source file
 source = "/Users/Josep/Desktop/SourceFolder/"
-
-
-
 #destination folder
-#
-#dest = "/Users/Josep/Desktop/EndFolder/"
+dest = "/Users/Josep/Desktop/EndFolder/"
 files = os.listdir(source)
+#command to see of list was created correctly
 print(files)
-
+#for loop that links list of files to directory(pFiles),instantiates a pTime that holds 24 hours ago, instantiates a variable (mTime) that finds when our files were edited last,
+#instantiates variable (cDateT) that changes epoch time to readable time stamp, and finally an if comparision decides whether or not to move files. 
 for i in files:
-    i = 0
-    files
-    print(path.getmtime(files[i]))
-          
-  
-
-
-
-
-
-
-"""print(modificationTime)
-now = datetime.now()
-for i in files:
-    time = path.getmtime(i)
-    if time < timedelta(hours=24):
-    #we are saying move the files represented by 'i' to their new destination
+    pFiles = os.path.join(source, i)
+    pTime = datetime.datetime.now()-timedelta(hours = 24)
+    mTime = os.path.getmtime(pFiles)
+    cDateT = datetime.datetime.fromtimestamp(mTime)
+    if pTime < cDateT:
         shutil.move(source+i, dest)
-    else:
-        pass
-        
-"""
-    
+       
+
